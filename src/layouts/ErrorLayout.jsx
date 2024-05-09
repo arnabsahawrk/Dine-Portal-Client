7aa2c6bd4990f4c0ffd102997cd24757dfd1fc2e
arnabw <arnabsahawrk@gmail.com>
2024-05-09 21:51:42 +0600

    initial commit

---
+++ src/layouts/ErrorLayout.jsx
@@ -0,0 +1,11 @@
+import ErrorPage from "../pages/ErrorPage";
+
+const ErrorLayout = () => {
+  return (
+    <main className="text-pretty">
+      <ErrorPage />
+    </main>
+  );
+};
+
+export default ErrorLayout;
